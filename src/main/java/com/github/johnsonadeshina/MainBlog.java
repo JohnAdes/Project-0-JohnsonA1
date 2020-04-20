b876f1029df791d0e83438764c2416759a260179
Johnson <adeshina.bode@gmail.com>
2020-04-20 13:32:28 -0500

    commit

--- src/main/java/com/github/johnsonadeshina/MainBlog.java
+++ src/main/java/com/github/johnsonadeshina/MainBlog.java
@@ -13,29 +13,34 @@ public class MainBlog {
         boolean readyToBlog = true;
 
         while(readyToBlog){
+
+            int id = 0;
+            ++id;
+
             System.out.println("Please enter your Blog Title: or 'Quit' to exit the program");
-            String title = in.next();
+            String title = in.nextLine();
             if(title.equalsIgnoreCase("Quit")) {
                 break;
             }
 
             System.out.println("Please enter the Author:");
-            String author = in.next();
+            String author = in.nextLine();
 
             System.out.println("Please enter your Blog Entry:");
 //            in.next();
-            String blogEntry = in.next();
-
+            String blogEntry = in.nextLine();
 
             //create log of blogposts
-            Blog blog = new Blog(title, author, blogEntry);
+            Blog blog = new Blog(id, title, author, blogEntry);
             blogPost.add(blog);
-        }
-        in.close();
+            }
+            in.close();
 
+        System.out.println("S/No." + ".\t " + "Title" + ",\t " + "Author" + ",\t " + "Blog Entry" );
+        System.out.println("==============================================");
         for(Blog blogs : blogPost){
-            System.out.println(blogs.getTitle() + ",\t " + blogs.getAuthor() + ",\t " + blogs.getBlogEntry());
-            System.out.println();
+            System.out.println(blogs.getId() + ",\t " + blogs.getTitle() + ",\t " + blogs.getAuthor() + ",\t " + blogs.getBlogEntry());
+
         }
 
     }
